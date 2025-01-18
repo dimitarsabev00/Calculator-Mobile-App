100813d3252cbca2084c43011184d3b21a5111c5
Dimitar Sabev <dimitarsabev00@gmail.com>
2025-01-18 09:47:35 +0200

    Added ThemeContext

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,35 @@
-import { Text, View } from "react-native";
+import { StyleSheet, Switch } from "react-native";
+import { ThemeContext } from "../contexts/ThemeContext";
+import { useState } from "react";
+import { Colors } from "@/styles/Colors";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
+  const [theme, setTheme] = useState("light");
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Calculator Mobile App!</Text>
-    </View>
+    <ThemeContext.Provider value={theme}>
+      <SafeAreaView
+        style={
+          theme === "light"
+            ? styles.container
+            : [styles.container, { backgroundColor: "black" }]
+        }
+      >
+        <Switch
+          value={theme === "dark"}
+          onValueChange={() => setTheme(theme === "light" ? "dark" : "light")}
+        />
+      </SafeAreaView>
+    </ThemeContext.Provider>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: Colors.light,
+    alignItems: "center",
+    justifyContent: "flex-start",
+  },
+});
